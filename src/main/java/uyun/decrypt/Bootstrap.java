c75fef082e5d892717c312810210087c44d41174
yuezq <rayv@foxmail.com>
2019-10-11 00:09:34 +0800

    first-commit

---
+++ src/main/java/uyun/decrypt/Bootstrap.java
@@ -0,0 +1,13 @@
+package uyun.decrypt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bootstrap {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bootstrap.class, args);
+    }
+
+}
