6d645d624a4c5b1783ee87a6293df445931e22d1
yuezq <rayv@foxmail.com>
2019-10-11 00:29:28 +0800

    新增解密接口

---
+++ src/main/java/uyun/decrypt/controller/DecryptController.java
@@ -0,0 +1,15 @@
+package uyun.decrypt.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 解密接口
+ *
+ * @author rayv
+ * @create 2019-10-11-0:28
+ */
+@RestController
+@RequestMapping
+public class DecryptController {
+}
